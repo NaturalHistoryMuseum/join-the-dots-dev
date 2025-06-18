f1909286fcbd4de81f1d67f7f5cae307efab7982
Ginger Burns <alycejenni@gmail.com>
2025-06-18 18:25:04 +0100

    chore: use generic eslint prettier config
    Removes ties to create-vue. The behaviour is the same (i.e. it doesn't format).

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
@@ -15,5 +15,5 @@ export default [
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  skipFormatting,
+  eslintConfigPrettier,
 ]
