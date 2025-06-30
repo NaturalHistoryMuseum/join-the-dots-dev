0fb5cb46fbe5e2649fc3cb3e4e9c3a31db0092bd
AndrewRoberts1 <andy.direct101@gmail.com>
2025-06-30 16:19:59 +0100

    refactor(flask-config): remove dot env import

--- server/config.py
+++ server/config.py
@@ -1,14 +1,7 @@
 import os
-from pathlib import Path
-
-from dotenv import load_dotenv
 
 
 class Config:
-    # Force load .env file in this directory
-    env_path = Path(__file__).resolve().parent / '.env'
-    load_dotenv(dotenv_path=env_path, override=True)
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Database Configuration
