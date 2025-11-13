00a956d15f4c3e51cf5e398b221f84df30d70821
AndrewRoberts1 <andy.direct101@gmail.com>
2025-11-13 17:19:03 +0000

    fix(help-page): resolve issue of md content not showing on help page
    Closes: #65

---
+++ src/components/help-content/view_units.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>
+    To view unit data, either use the 'View Units' tab in the nav bar or click
+    on the 'View units' action tab from the homepage.
+  </p>
+  <p>
+    Down the left-hand side of the screen, you'll see a range of filters to help
+    you find the data you need.
+  </p>
+  <p>
+    If you are responsible for certain units, these will automatically be
+    displayed. If you wish to search all units, simply uncheck the 'Show only my
+    assigned units' box at the top of the Filters menu.
+  </p>
+  <p>
+    Select the unit you want to view and use the subsequent tabs to navigate
+    through the information: Unit details, Properties, Storage, and Scores. The
+    information fields displayed under these tabs may vary slightly depending on
+    the department the unit belongs to.
+  </p>
+  <p>
+    Most recent scores can only be viewed in this workflow. If you wish to edit
+    scores for your units, that can be done using the 'Rescore' tab in the nav
+    bar or from the homepage.
+  </p>
+</template>
